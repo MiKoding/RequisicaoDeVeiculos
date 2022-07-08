1ef97f43a6048b44b803577827ec003e6cd5ef4a
MiKoding <mikaioharison038@gmail.com>
2022-07-07 21:54:40 -0300

    Criando primeiros controllers

--- RequisicaoDeVeiculos/Controllers/HomeController.cs
+++ RequisicaoDeVeiculos/Controllers/HomeController.cs
@@ -1,20 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RequisicaoDeVeiculos.Models;
 using System.Diagnostics;
+//using System;
+//using System.Data.Entity;
+//using System.Linq;
+//using System.Net;
+//using System.Data.Entity.Validation;
+//using System.Collections.Generic;
+//using System.Threading.Tasks;
+//using System.Net.Mail;
+//using System.Configuration;
+//using System.Data.Entity.Migrations;
+
 
 namespace RequisicaoDeVeiculos.Controllers
 {
+   
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        //private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
 
         public IActionResult Index()
         {
+            
             return View();
         }
 
