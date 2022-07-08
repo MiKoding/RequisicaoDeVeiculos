056e52c2c96fc46c4c67b43e9aa37a1c90016381
MiKoding <mikaioharison038@gmail.com>
2022-07-07 21:55:15 -0300

    Models criados para implementação da requisição

---
+++ RequisicaoDeVeiculos/Models/Requisicao.cs
@@ -0,0 +1,15 @@
+﻿namespace RequisicaoDeVeiculos.Models
+{
+    public class Requisicao
+    {
+        public int Id { get; set; }
+        public string Solicitante { get; set; }
+        public string Local { get; set; }
+                                            
+        public string Veiculo { get; set; }
+
+        public string SituacaoRequisicao { get; set; }
+        public DateTime DataSaida { get; set; }   
+        public DateTime DataEntrada { get; set; }
+    }
+}
---
+++ RequisicaoDeVeiculos/Models/UsuarioModel.cs
@@ -0,0 +1,12 @@
+﻿namespace RequisicaoDeVeiculos.Models
+{
+    public class UsuarioModel
+    {
+        public int id { get; set; }
+        public string Usuario { get; set; }
+        
+        public string Senha { get; set; }   
+
+        public int Permissao { get; set; }
+    }
+}
